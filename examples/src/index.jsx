bf470d5a04b0c7714c586f7ef78826147d41e85d
Brandon Mills <mills.brandont@gmail.com>
2015-11-27 00:51:31 -0500

    Make example direction configurable

--- examples/src/index.jsx
+++ examples/src/index.jsx
@@ -1,6 +1,7 @@
 import lorem from 'lorem-ipsum';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import RadioGroup from 'react-radio-group';
 
 import SplitView from '../../src';
 
@@ -8,13 +9,39 @@ const App = React.createClass({
 
 	displayName: 'App',
 
+	getInitialState() {
+		return {
+			direction: 'row'
+		};
+	},
+
+	handleChangeDirection(direction) {
+		this.setState({ direction });
+	},
+
 	render() {
 		return (
 			<div className="App">
-				<SplitView>
-					<p>{lorem({ count: Math.random() * 8 })}</p>
-					<p>{lorem({ count: Math.random() * 8 })}</p>
-				</SplitView>
+				<aside className="App-sidebar">
+					<RadioGroup
+						name="direction"
+						onChange={this.handleChangeDirection}
+						selectedValue={this.state.direction}
+					>
+						{Radio => (
+							<div>
+								<label><Radio value="column" />Column</label>
+								<label><Radio value="row" />Row</label>
+							</div>
+						)}
+					</RadioGroup>
+				</aside>
+				<div className="App-example">
+					<SplitView direction={this.state.direction}>
+						<p>{lorem({ count: Math.random() * 8 })}</p>
+						<p>{lorem({ count: Math.random() * 8 })}</p>
+					</SplitView>
+				</div>
 			</div>
 		);
 	}
